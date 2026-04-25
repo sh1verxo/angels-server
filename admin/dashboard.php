12fcbee5cba32d1e7e69c7023980a4c9722e0051
Vraudrak <mmmnawh@gmail.com>
2026-04-25 12:08:34 -0700

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,14 +1,15 @@
 <?php
 session_start();
-
 if (!isset($_SESSION['auth'])) {
     header("Location: ../login.php");
     exit;
 }
 ?>
 
-<h1>Admin Dashboard</h1>
+<h1>Admin Panel</h1>
 
-<p>Logged in successfully.</p>
-
-<a href="../logout.php">Logout</a>
+<ul>
+  <li><a href="edit.php">Edit Site Content</a></li>
+  <li><a href="sql.php">SQL Viewer</a></li>
+  <li><a href="../logout.php">Logout</a></li>
+</ul>
